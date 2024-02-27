db65a0d2790cfdf42a7a37095fb2f48491dc8efd
JI-0 <janivkovic11111@gmail.com>
2024-02-28 00:32:07 +0900

    /

--- sqisign.go
+++ sqisign.go
@@ -1,7 +1,7 @@
 package sqisign
 
 //#cgo CFLAGS: -I/usr/local/include -I/opt/local/include -I/usr/include -I/opt/homebrew/opt/gmp/include
-//#cgo LDFLAGS: -L/usr/local/lib -L/opt/local/lib -L/usr/lib -Lbuilds/ref -L. -Lbuild/src -lsqisign_lvl1_nistapi
+//#cgo LDFLAGS: -L/usr/local/lib -L/opt/local/lib -L/usr/lib -L. -Lbuild/src -lsqisign_lvl1_nistapi
 //#cgo LDFLAGS: -lsqisign_lvl1 -Lbuild/src/protocols/ref/lvl1 -lsqisign_protocols_lvl1 -Lbuild/src/precomp/ref/lvl1
 //#cgo LDFLAGS: -lsqisign_precomp_lvl1 -Lbuild/src/klpt/ref/lvl1 -lsqisign_klpt_lvl1 -Lbuild/src/quaternion/ref/generic
 //#cgo LDFLAGS: -lsqisign_quaternion_generic -Lbuild/src/id2iso/ref/lvl1 -lsqisign_id2iso_lvl1 -Lbuild/src/intbig/ref/generic
@@ -13,6 +13,8 @@ import "C"
 import (
 	"unsafe"
 )
+
+// -Lbuilds/ref
 
 const SecretKeyLen = C.CRYPTO_SECRETKEYBYTES
 const PublicKeyLen = C.CRYPTO_PUBLICKEYBYTES
@@ -41,7 +43,9 @@ func SQIGenerateKeypair() ([]byte, []byte) {
 	priv := C.CBytes(make([]byte, SecretKeyLen))
 	defer C.free(unsafe.Pointer(priv))
 
-	_ = C.sqisigngo_gen_keypair(pub, priv)
+	if err := C.sqisigngo_gen_keypair(pub, priv); int(err) != 0 {
+		println(int(err))
+	}
 
 	return C.GoBytes(pub, PublicKeyLen), C.GoBytes(priv, SecretKeyLen)
 }
@@ -56,9 +60,9 @@ func SQISign(priv, msg string) []byte {
 	msgC := C.CString(msg)
 	defer C.free(unsafe.Pointer(msgC))
 
-	if err := C.sqisigngo_sign(sig, msgC, privC); int(err) != 0 {
-		panic("error sqisigngo_sign")
-	}
+	_ = C.sqisigngo_sign(sig, msgC, privC) //; int(err) != 0 {
+	// 	panic("error sqisigngo_sign")
+	// }
 
 	return C.GoBytes(sig, C.int(SignatureLen+len(msg)))[:SignatureLen]
 }
